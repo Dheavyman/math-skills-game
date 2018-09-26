ee21112f21f1f487aec58ff69fb48ffeb5da8a2d
Justin Nebo <nebojustin@gmail.com>
2018-09-26 20:10:49 +0100

    feat(interface): build game interface - build components for the stars button and numbers - add function to generate random number of stars

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Game from './components/Game';
+import '../public/style.scss';
+
+/**
+ * Main app component
+ *
+ * @returns {object} React element
+ */
+const App = () => (
+  <div>
+    <Game />
+  </div>
+);
+
+export default App;
---
+++ src/components/Answer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/**
+ * Answer component
+ *
+ * @returns {object} React element
+ */
+const Answer = () => (
+  <div className="col-5">
+    <div className="number">
+      <span>2</span>
+      <span>6</span>
+    </div>
+  </div>
+);
+
+export default Answer;
